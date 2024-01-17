7dbc7ad9a385f1d81289615277e39c292c56e5d3
Ekaterinn1 <146169897+Ekaterinn1@users.noreply.github.com>
2024-01-17 15:48:40 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # vm22learnexpress
+ `node server.js`
+
+## install
+`npm i`
+
+## commands
+- `cd` enter folder
+- `cd .` exit folder
+- `git clone 'link' ` clone from github
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const nunjucks = require('nunjucks')
+const app = express();
+const port = 3000;
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+app.get('/page2', (req, res) => {
+    res.render('page2.html');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
